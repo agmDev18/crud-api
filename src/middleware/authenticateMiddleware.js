402e38e8a28ea4cc61b4e2110a52559071019a3d
germano <germano@debian12.debian12>
2024-04-02 23:43:42 -0300

    create middleware to verify current user

--- src/middleware/authenticateMiddleware.js
+++ src/middleware/authenticateMiddleware.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const { sign, verify } = require('jsonwebtoken');
 
+const User = require('../models/user.model');
+
 const SECRET = process.env.SECRET
 
 const createTokens = (user) => {
@@ -27,7 +29,25 @@ const validateToken = (req, res, next) => {
     }
 };
 
+const checkCurrentUser = (req, res, next) => {
+    const token = req.cookies['access-token'];
+
+    if (token) {
+        verify(token, SECRET, async(err, decodedToken) => {
+            if (err) {
+                res.locals.user = null;
+                throw new Error(err.message);
+            } else {
+                const user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        });
+    }
+};
+
 module.exports = {
     createTokens,
-    validateToken
+    validateToken,
+    checkCurrentUser
 };
